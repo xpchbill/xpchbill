16433ed73bb79f3334310faac1a23f1f8298b8f6
Bill Xiong <bill.xiong@activenetwork.com>
2017-02-14 23:20:13 +0800

    Modify sass loader for loding office-ui-fabric-react normally.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,31 @@
-const path = require('path');
+import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
-exports.modifyWebpackConfig = (webpackConfig) => {
+exports.modifyWebpackConfig = (webpackConfig, env) => {
+  webpackConfig.loader('source-map-loader', (cfg) => {
+    const config = cfg;
+    config.test = /\.js$/;
+    config.loader = 'source-map-loader';
+    return config;
+  });
   webpackConfig.loader('sass', (cfg) => {
     const config = cfg;
     config.test = /\.(sass|scss)/;
-    config.loader = 'style!css!postcss!sass';
-    config.exclude = [
-      /\.module\.(sass|scss)$/,
-      path.resolve('node_modules/office-ui-fabric-react/lib')
-    ];
+    switch (env) {
+      case 'develop':
+        config.loader = 'style!css!postcss!sass';
+        config.exclude = [
+          /\.module\.(sass|scss)$/,
+          path.resolve('node_modules/office-ui-fabric-react/lib')
+        ];
+        break;
+      case 'build-javascript':
+        config.test = /\.scss$/;
+        config.loader = 'null';
+        break;
+      default:
+        break;
+    }
     return config;
   });
   return webpackConfig;
